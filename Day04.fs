b3a1d0e414be2e826f13cf540b46a1a84a9d8a75
Felipe Rabuske Costa <felipe.costa@sap.com>
2021-05-28 18:01:44 -0300

    Day 04

---
+++ Day04.fs
@@ -0,0 +1,37 @@
+﻿module Day04
+
+let hasAtLeastTwoAdjacentDigits number =
+    let numberAsChars = Array.toList <| number.ToString().ToCharArray()
+    let tail = List.tail numberAsChars
+    let tailess = List.truncate (numberAsChars.Length - 1) numberAsChars
+    List.zip tail tailess
+        |> List.exists(fun(zipped) -> 
+            fst zipped = snd zipped
+        )
+
+let listRepeatedNumbers number =
+    let numberAsChars = Array.toList <| number.ToString().ToCharArray()
+    numberAsChars 
+    |> List.fold (fun (accum: char list list) (x: char) ->
+        match accum with
+        | head :: tail ->
+            if (List.head head) = x then (x :: head) :: tail
+            else [x] :: (head :: tail)
+        | [] -> [[x]]
+    ) []
+        
+let hasOnlyPairOfDigits number =
+    listRepeatedNumbers number |> List.exists (fun (chunk) -> chunk.Length = 2)
+
+let onlyIncreasingDigits number = 
+    let numberAsString = Array.toList <| number.ToString().ToCharArray()
+    numberAsString = List.sort numberAsString
+
+let processInput number1 number2 (rules: (int->bool) list) =
+    seq {
+        for number in [number1 .. number2] do
+            rules |> List.fold (fun (valid: bool) (rule: int->bool) ->
+                rule(number) && valid
+            ) true 
+    } |> Seq.filter(fun(result) -> result)
+    |> Seq.length
--- Program.fs
+++ Program.fs
@@ -4,7 +4,10 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    Day03.processInputPart2 |> printfn "%f"
+    //Day04.processInput 372037 905157 [Day04.hasOnlyPairOfDigits; Day04.onlyIncreasingDigits] |> printfn "%i"
+    //Day04.processInput 372037 905157 [Day04.hasAtLeastTwoAdjacentDigits; Day04.onlyIncreasingDigits] |> printfn "%i"
+    //Day04.processInputPart1 372037 905157 |> printfn "%i"
+    //Day03.processInputPart2 |> printfn "%f"
     //Day03.processInputPart1 |> printfn "%f" 
     //Day02.processInputPart2 |> printfn "%i" 
     //Day02.processInputPart1 |> printfn "%i" 
