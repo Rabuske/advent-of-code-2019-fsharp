c4184799c6cb92174e385f5260fd3c1e2f38075e
Felipe Rabuske Costa <felipe.costa@sap.com>
2021-06-22 22:05:28 -0300

    Day06

---
+++ Day06.fs
@@ -0,0 +1,34 @@
+﻿module Day06
+
+type PlanetAndOrbit = {
+    Planet: string;
+    Orbit: string
+}
+
+let parseIntoOrbits (line:string) =
+    let obj = line.Split(")")
+    obj.[1], obj.[0]
+
+let buildPlanetsDictionary lines = 
+    lines 
+        |> Seq.map parseIntoOrbits 
+        |> Map.ofSeq
+
+let pathToRoot (orbits: Map<string, string>) (planet: string) =
+    let rec pathToRootClosure planet = 
+        match Map.tryFind planet orbits with
+            | Some p -> p :: pathToRootClosure p
+            | None -> [ ]
+    pathToRootClosure planet
+
+let processPart01 = 
+    let orbits = buildPlanetsDictionary <| Utils.readLines "06"
+    orbits |> Map.toSeq |> Seq.sumBy (fst >> pathToRoot orbits >> List.length)
+
+let processPart02 = 
+    let orbits = buildPlanetsDictionary <| Utils.readLines "06"
+    let youToRoot = pathToRoot orbits "YOU" |> List.rev
+    let santaToRoot = pathToRoot orbits "SAN" |> List.rev
+    
+    let commomDistance = Seq.zip youToRoot santaToRoot |> Seq.findIndex (fun (planet1, planet2) -> planet1 <> planet2)
+    youToRoot.Length + santaToRoot.Length - (2 * commomDistance)
--- Program.fs
+++ Program.fs
@@ -4,6 +4,8 @@ open System
 
 [<EntryPoint>]
 let main argv =
+    Day06.processPart02 |> printfn "%i"
+    //Day06.processPart01 |> printfn "%i"
     //Day04.processInput 372037 905157 [Day04.hasOnlyPairOfDigits; Day04.onlyIncreasingDigits] |> printfn "%i"
     //Day04.processInput 372037 905157 [Day04.hasAtLeastTwoAdjacentDigits; Day04.onlyIncreasingDigits] |> printfn "%i"
     //Day04.processInputPart1 372037 905157 |> printfn "%i"
